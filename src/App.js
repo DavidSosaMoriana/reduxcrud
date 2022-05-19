586d881fb307d31f2a051b1d35cb0d63be84edae
David Sosa <david.sosa@singularfactory.com>
2022-05-19 21:05:00 +0100

    Add Modal Functionality

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Navbar from "./components/Navbar";
 import CartContainer from './components/CartContainer';
 import { useSelector, useDispatch } from 'react-redux';
 import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
  
 
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
@@ -14,6 +16,7 @@ function App() {
 
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
